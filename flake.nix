0f9293c0825ae563b276ab8278d382e1788ba946
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-18 00:15:11 +0400

    added flake.nix to facilitate nix flake init/new

---
+++ flake.nix
@@ -0,0 +1,15 @@
+# lets you initialize the template fairly easily with
+# nix flake new proj -t github:Rexcrazy804/npins-template
+{
+  outputs = _: {
+    templates.default = {
+      path = ./.;
+      descprition = "An elegant flake like npins template";
+      welcomeText = ''
+        # Rexiel's npins template
+        See the justfile for common commands to build 
+        and use the default.nix.
+      '';
+    };
+  };
+}
