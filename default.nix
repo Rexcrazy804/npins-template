7c661c50257b6a8eeaff8885ce20c89724142fd5
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-18 14:58:33 +0400

    addition of nixosConfigurations

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@
 }:
 pkgs.lib.fix (self: let
   inherit (pkgs.lib) callPackageWith;
+  inherit (pkgs) nixos;
   callPackage = callPackageWith (pkgs // self.packages);
 in {
   formatter = pkgs.alejandra;
@@ -13,10 +14,15 @@ in {
     cowask = callPackage ./nix/pkgs/cowask.nix {};
     lolask = callPackage ./nix/pkgs/lolask.nix {};
     default = self.packages.cowask;
+    cowVM = self.nixosConfigurations.cowHost.config.system.build.vm;
   };
 
   devShells = {
     cowShell = callPackage ./nix/shells/cowShell.nix {};
     default = self.devShells.cowShell;
+  };
+
+  nixosConfigurations = {
+    cowHost = nixos [./nix/hosts/cowHost/configuration.nix];
   };
 })
